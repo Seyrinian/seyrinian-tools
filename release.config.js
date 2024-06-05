e96f680622cf85b73ff34f65070bc856a6a7fa6f
Valentin Fourny <fourny.valentin@gmail.com>
2024-06-05 16:40:28 +0200

    Feature/init project (#1)
    * feat: init project
    
    * fix: package number
    
    ---------
    
    Co-authored-by: vfourny <valentin.fourny@gmail.com>

---
+++ release.config.js
@@ -0,0 +1,34 @@
+export default {
+  branches: ['main', { name: 'next', prerelease: true }],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: process.env.PUBLISH === 'true',
+        pkgRoot: '.',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['docs/CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
+    ],
+  ],
+};

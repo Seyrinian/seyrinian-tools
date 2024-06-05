1daad2b3d0008dc80edaa2c6f4edc4e65ab16dc3
Valentin Fourny <fourny.valentin@gmail.com>
2024-06-05 18:04:52 +0200

    Fix/readme (#4)
    * Feature/init project (#1)
    
    * feat: init project
    
    * fix: package number
    
    ---------
    
    Co-authored-by: vfourny <valentin.fourny@gmail.com>
    
    * fix: update Readme.md
    
    * fix: update Readme.md (#3)
    
    Co-authored-by: vfourny <valentin.fourny@gmail.com>
    
    * chore(release): 1.0.0-next.1 [skip ci]
    
    # 1.0.0-next.1 (2024-06-05)
    
    ### Bug Fixes
    
    * update Readme.md ([#3](https://github.com/Seyrinian/seyrinian-tools/issues/3)) ([464675b](https://github.com/Seyrinian/seyrinian-tools/commit/464675b9c585dab161798e4b3d7388e659af193d))
    
    ---------
    
    Co-authored-by: vfourny <valentin.fourny@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.0.0-next.1 (2024-06-05)
+
+
+### Bug Fixes
+
+* update Readme.md ([#3](https://github.com/Seyrinian/seyrinian-tools/issues/3)) ([464675b](https://github.com/Seyrinian/seyrinian-tools/commit/464675b9c585dab161798e4b3d7388e659af193d))
---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    name: 'eslint-config-seyrinian-config',
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      // Add your own rules here
+      'no-const-assign': 'warn',
+      'no-constant-condition': 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist/'],
+  },
+];

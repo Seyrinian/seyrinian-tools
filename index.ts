6f82a9905ea97902fc7dce67f3af4a427f480366
vfourny <valentin.fourny@gmail.com>
2024-06-10 23:00:54 +0200

    fix: a dd index.ts

---
+++ index.ts
@@ -0,0 +1,15 @@
+import eslint from '~/eslint.config';
+import prettier from '~/prettier.config';
+import renovate from '~/renovate.json';
+import tsConfig from '~/tsconfig.json';
+import commitlint from '~/commitlint.config';
+import semanticRelease from '~/release.config';
+
+export default {
+  eslint,
+  prettier,
+  renovate,
+  tsConfig,
+  commitlint,
+  semanticRelease,
+};

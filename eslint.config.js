a0f2575d929d55774802db6cc3e878de4fe316b5
vfourny <valentin.fourny@gmail.com>
2024-06-11 13:22:08 +0200

    fix: add tslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-import js from '@eslint/js';
+import eslintJS from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
-export default [
-  js.configs.recommended,
+export default tseslint.config(
+  eslintJS.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   {
     name: 'eslint-config-seyrinian-config',
     languageOptions: {
@@ -19,5 +23,5 @@ export default [
   },
   {
     ignores: ['node_modules', 'dist/'],
-  },
-];
+  }
+);

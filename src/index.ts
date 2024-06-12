749ec8a11995e5881430c80cfe45ac408a4a977d
vfourny <valentin.fourny@gmail.com>
2024-06-13 01:00:56 +0200

    fix: update vite

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import eslintConfig from '../eslint.config.js';
+import prettierConfig from '../prettier.config.js';
+import renovateConfig from '../renovate.json';
+import tsConfig from '../tsconfig.json';
+import commitlintConfig from '../commitlint.config.js';
+import semanticReleaseConfig from '../release.config.js';
+
+export default {
+  eslintConfig,
+  prettierConfig,
+  renovateConfig,
+  tsConfig,
+  commitlintConfig,
+  semanticReleaseConfig,
+};
